cc7799577b8fe923ecc229209ff20afdcf5f6ff7
Fuat Can ERYİĞİT <fceryigit03@gmail.com>
2022-07-07 16:55:59 +0300

    level loading

--- RunForYourColor/Assets/Scripts/LevelLoader/SO_LevelLoader.cs
+++ RunForYourColor/Assets/Scripts/LevelLoader/SO_LevelLoader.cs
@@ -6,6 +6,18 @@ namespace RedPanda
     [CreateAssetMenu(fileName = "LevelLoader", menuName = "Red Panda/Level Loader")]
     public class SO_LevelLoader : ScriptableObject
     {
+        public void LevelLoad()
+        {
+            int index = SceneManager.GetActiveScene().buildIndex + 1;
+
+            if (index > SceneManager.sceneCount)
+            {
+                index = SceneManager.sceneCount;
+            }
+
+            SceneManager.LoadScene(index);
+        }
+
         public void LoadLevel(string levelName)
         {
             SceneManager.LoadScene(levelName);
